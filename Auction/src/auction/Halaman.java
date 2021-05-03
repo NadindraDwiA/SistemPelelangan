e99371a3e991ef6c5b4235c2f1e1ad798a7da337
NadindraDwiA <83199349+NadindraDwiA@users.noreply.github.com>
2021-05-03 16:12:55 +0700

    Add files via upload

---
+++ Auction/src/auction/Halaman.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auction;
+
+interface Halaman {
+    
+    void add();
+    void delete();
+    void edit();
+    void detail();
+    void GetTable();
+    void DisplayTable();
+    void FetchPhoto();
+}
