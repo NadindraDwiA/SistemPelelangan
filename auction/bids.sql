06fbc941b8eb89880e67ecbebb179e59f9f0b5b9
NadindraDwiA <83199349+NadindraDwiA@users.noreply.github.com>
2021-05-03 16:13:43 +0700

    Add files via upload

---
+++ auction/bids.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Waktu pembuatan: 03 Bulan Mei 2021 pada 11.13
+-- Versi server: 8.0.24
+-- Versi PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `auction`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `bids`
+--
+
+CREATE TABLE `bids` (
+  `nama` varchar(100) NOT NULL,
+  `alamat` text NOT NULL,
+  `harga_tawar` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `bids`
+--
+ALTER TABLE `bids`
+  ADD PRIMARY KEY (`nama`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
